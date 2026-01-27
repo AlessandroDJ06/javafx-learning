183f5eac82230fd48caab9f66fddcaadbf70132c
AlessandroDJ06 <alessandrodejongh@outlook.com>
2026-01-27 21:28:04 +0100

    [HelloApplication]stage experimenting

--- src/main/java/com/example/javafxlearning/HelloApplication.java
+++ src/main/java/com/example/javafxlearning/HelloApplication.java
@@ -1,19 +1,33 @@
 package com.example.javafxlearning;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        Image icon = new Image(HelloApplication.class.getResource("/images/img.png").toExternalForm());
+        stage.getIcons().add(icon);
+
+        stage.setTitle("testJavaFx");
+        stage.setWidth(420);
+        stage.setHeight(420);
         stage.setScene(scene);
+
+//        stage.setX(50);
+//        stage.setY(50);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("YOU CANT ESCAPE unless you press q");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
         stage.show();
     }
 }
+
