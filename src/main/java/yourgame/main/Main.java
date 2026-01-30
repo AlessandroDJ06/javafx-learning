7f2cdbaf2a1e579807a3b3bf1f2c506fd9700559
AlessandroDJ06 <alessandrodejongh@outlook.com>
2026-01-30 23:44:53 +0100

    [HelloApplication]router added + initial test passed with succes

--- src/main/java/yourgame/main/Main.java
+++ src/main/java/yourgame/main/Main.java
@@ -2,16 +2,20 @@ package yourgame.main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import yourgame.core.Router;
+import yourgame.core.Scenetype;
 
 public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Router router = new Router(stage);
+        stage.setTitle("TestApplication");
+        router.goTo(Scenetype.MENU);
+    }
+
     public static void main(String[] args){
         launch(args);
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("TestApplication");
-        stage.show();
 
-    }
 }
